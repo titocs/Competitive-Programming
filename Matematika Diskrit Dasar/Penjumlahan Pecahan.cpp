737ee9db11825c6f278b1e8718015506d2441926
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 06:52:15 +0700

    Add files via upload

---
+++ Matematika Diskrit Dasar/Penjumlahan Pecahan.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fpb(a, b) __gcd(a, b)
+typedef long long ll;
+
+int main(){
+	ll a, b, c, d, e, f;
+	cin >> a >> b >> c >> d;
+	ll pembilang = a*d + c*b;
+	ll penyebut = b*d;
+	ll div = fpb(pembilang, penyebut);
+	cout << pembilang/div << " " << penyebut/div;
+}

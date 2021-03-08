97f972ecd6d06955ec675fe916000f037bacc615
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 09:01:27 +0700

    Update Stack and Queue.cpp

--- Struktur Data Dasar/Stack and Queue.cpp
+++ Struktur Data Dasar/Stack and Queue.cpp
@@ -6,20 +6,19 @@ int main(){
     int n, angka;
     cin >> n;
     string s;
-	
     for(int i=0; i<n; ++i){
         cin >> s;
-		    if(s == "push_back" || s == "push_front")
-			      cin >> angka;
-		    if(s == "push_back")
-			      v.push_back(angka);
-		    if(s == "push_front")
-			      v.push_front(angka);
-		    if(s == "pop_back")
-			      v.pop_back();
-		    if(s == "pop_front")
-			      v.pop_front();
-	  }
+        if(s == "push_back" || s == "push_front")
+            cin >> angka;
+        if(s == "push_back")
+            v.push_back(angka);
+        if(s == "push_front")
+            v.push_front(angka);
+        if(s == "pop_back")
+            v.pop_back();
+        if(s == "pop_front")
+            v.pop_front();
+    }
     for(int i=0; i<v.size(); ++i)
-		    cout << v[i] << endl;
+        cout << v[i] << endl;
 }

11c28e12b9320a394dfb1564655bfbc49fd5cad3
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 11:54:18 +0700

    Update Prima ke-K.cpp

--- Matematika Diskrit Dasar/Prima ke-K.cpp
+++ Matematika Diskrit Dasar/Prima ke-K.cpp
@@ -4,31 +4,27 @@ using namespace std;
 #define MAX_SIZE 1000005
  
 void SieveOfEratosthenes(vector<int>& primes){
-    bool IsPrime[MAX_SIZE];
-    memset(IsPrime, true, sizeof(IsPrime));
- 
-    for (int p = 2; p * p < MAX_SIZE; p++) {
-       if (IsPrime[p] == true) {
-          for (int i = p * p; i < MAX_SIZE; i += p)
-             IsPrime[i] = false;
-         }
+    bool cekprime[MAX_SIZE];
+    memset(cekprime, true, sizeof(cekprime));
+    for(int p = 2; p * p < MAX_SIZE; p++){
+        if (cekprime[p] == true) {
+            for(int i = p * p; i < MAX_SIZE; i += p)
+                cekprime[i] = false;
+        }
     }
- 
-    for (int p = 2; p < MAX_SIZE; p++)
-       if (IsPrime[p])
-          primes.push_back(p);
+    for(int p = 2; p < MAX_SIZE; p++)
+        if(cekprime[p])
+            primes.push_back(p);
 }
- 
+
 int main(){
     vector<int> primes;
     SieveOfEratosthenes(primes);
-   
     int banyak;
     cin >> banyak;
     for(int i=0; i<banyak; ++i){
-       int n;
-       cin >> n;
-       cout << primes[n-1] << endl;
+        int n; cin >> n;
+        cout << primes[n-1] << endl;
     }
     return 0;
 }

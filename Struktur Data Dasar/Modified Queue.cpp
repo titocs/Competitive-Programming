8481b9b8cc4af797c01e766a8affc1482d3be99d
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 07:49:40 +0700

    Create Modified Queue.cpp

---
+++ Struktur Data Dasar/Modified Queue.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main(){
+    ll n, x, y, temp;
+    cin >> n;
+    string s;
+    deque<int> stack;
+	  for(int i=0; i<n; ++i){
+        cin >> s;
+		    if(s == "add"){
+            cin >> x >> y;
+			      for(int j=0; j<y; ++j)
+				        stack.push_back(x);
+			      cout << stack.size() << endl;
+		    }
+		    if(s == "del"){
+			      cin >> y;
+			      temp = stack[0];
+			      for(int j=0; j<y; ++j)
+				        stack.pop_front();
+			      cout << temp << endl;
+		    }
+		    if(s == "rev")
+			      reverse(stack.begin(), stack.end());
+	 }
+}

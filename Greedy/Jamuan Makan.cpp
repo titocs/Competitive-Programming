0dab9a71a66c8dfa03cba24a4b01388ac6a3406b
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-28 13:46:08 +0700

    Create Jamuan Makan.cpp

---
+++ Greedy/Jamuan Makan.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    vector<pair<int, int>> arr;
+    for(int i=0; i<n; ++i){
+        int x, y; cin >> x >> y;
+        int akhir = x + y;
+        arr.push_back(make_pair(x, akhir));
+    }
+    sort(arr.begin(), arr.end(), [&](pair<int, int> a, pair<int, int>b){
+        return a.second < b.second;
+    });
+    int selected_count = 0;
+    int start = 0;
+    for(int i=0; i<n; ++i){
+        if(arr[i].first >= start){
+            selected_count++;
+            start = arr[i].second;
+        }
+    }
+    cout << selected_count++;
+}
